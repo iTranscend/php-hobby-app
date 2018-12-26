1c0c7f85f91b6a252a41bc0d49e23181771a0be9
Femi <llordbanks500@gmail.com>
2018-12-26 16:50:01 +0100

    Removed strict empty-fields validation from main.js and included empty-fields validation in signUp.html

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
 function signUpValidate() {
-    if ( document.signUpForm.firstname.value == "" ) {
-        alert("Please Enter your firstname");
-        document.signUpForm.firstname.focus();
-        return false;
-    }
-    if ( document.signUpForm.lasname.value == "" ) {
-        alert("Please Enter your Lastname");
-        document.signUpForm.lasname.focus();
-        return false;
-    }
-    if ( document.signUpForm.email.value == "" ) {
-        alert("Please Enter your email");
-        document.signUpForm.email.focus();
-        return false;
-    }
-    if ( document.signUpForm.passkey.value == "" ) {
-        alert("Please Enter your password");
-        document.signUpForm.passkey.focus();
-        return false;
-    }
     if ( document.signUpForm.passkeyconf.value == "" || ( document.signUpForm.passkeyconf.value !== document.signUpForm.passkey.value)) {
         alert("Please ensure that your confirmation password is the same as your original password");
         document.signUpForm.passkeyconf.focus();
